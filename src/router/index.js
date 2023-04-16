745006af38069e4678f22091bd19fac931d0a927
AntonioMance <amance@unipu.hr>
2023-04-16 17:58:15 +0200

    Promjena ruta

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,10 @@ import GameList from '../views/GameList.vue'
 import UserProfile from "../views/UserProfile.vue";
 import ChatRoom from '../views/ChatRoom.vue'
 import GameChat from '../views/GameChat.vue';
+import RegisterPage from '../views/RegisterPage.vue'
+import LoginPage from '../views/LoginPage.vue'
+import MainPage from '../views/MainPage.vue'
+import MyProfile from '../views/MyProfile.vue'
 
 const routes = [
   {
@@ -15,39 +19,27 @@ const routes = [
   {
     path: '/register',
     name: 'register',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/RegisterPage.vue')
+    component: RegisterPage
   },
   {
     path: '/login',
     name: 'login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/LoginPage.vue')
+    component: LoginPage
   },
   {
     path: '/mainpage',
     name: 'mainpage',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/MainPage.vue')
+    component: MainPage
   },
   {
     path: '/myprofile',
     name: 'myprofile',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/MyProfile.vue')
+    component: MyProfile
   },
   {
     path: '/generalchat',
     name: 'GeneralChat',
-    component: GeneralChat,
+    component: GeneralChat
   },
   {
     path: '/gamelist',
